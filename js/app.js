d977b4502ff8642ceaf32ec76c63ea382f5e7d74
Guillaume Gomez <guillaume.gomez@photobox.com>
2016-05-24 17:34:03 +0200

    Update cards to use JIRA REST API and be more readable

--- js/app.js
+++ js/app.js
@@ -1,58 +1,50 @@
 'use strict';
 
-function parseXML (fileContent) {
-    let domParser = new DOMParser();
-    let xml = domParser.parseFromString(fileContent, "text/xml");
-
-    let items = Array.from(xml.getElementsByTagName('item'));
-
-    return items.map((item) => {
-        let attributes = {};
-        attributes.key = item.getElementsByTagName('key')[0].innerHTML;
-        attributes.title = item.getElementsByTagName('summary')[0].innerHTML;
-        attributes.description = item.getElementsByTagName('description')[0].innerHTML;
-        let timeestimate = item.getElementsByTagName('timeestimate')[0];
-        attributes.timeestimate = timeestimate ? timeestimate.getAttribute('seconds') : null;
-
-        /* Custom fields */
-        let customFields = Array.from(item.getElementsByTagName('customfields')[0].children);
-        customFields.forEach((customField) => {
-            if (customField.id === 'customfield_12310293') {
-                attributes.effort = parseInt(customField.getElementsByTagName('customfieldvalue')[0].innerHTML);
-            }
-            if (customField.id === 'customfield_12311120') {
-                attributes.epicLink = customField.getElementsByTagName('customfieldvalue')[0].innerHTML;
-            }
-        });
-        return attributes;
-    })
-}
-
-function onUpload (event) {
-    let fileList = Array.from((event && event.dataTransfer) ? event.dataTransfer.files : $fileInput.files);
-
-    fileList.forEach((file) => {
-        let fileReader = new FileReader();
-        fileReader.onloadend = (e) => {
-            jiras.add(parseXML(e.target.result));
-        };
-        fileReader.readAsText(file);
-    });
-}
+localStorage.setItem('storyPointsFieldId', '');
 
 class JiraModel {
     constructor (attributes) {
-        this.key = attributes.key;
-        this.title = attributes.title;
-        this.description = attributes.description;
-        this.timeestimate = attributes.timeestimate ? attributes.timeestimate : 0;
-        this.effort = attributes.effort ? attributes.effort : 0;
-        this.epicLink = attributes.effort ? attributes.epicLink : null;
-        console.log(this);
+        this.parse(attributes);
+        this.blocks = [];
+        this.isBlockedBy = [];
     }
 
-    static isValid (attributes) {
-        return attributes && attributes.key && attributes.title;
+    parse (attributes) {
+        this.key = attributes.key || this.key;
+        this.summary = attributes.summary || this.summary;
+        this.description = attributes.description || this.description;
+        this.timeestimate = attributes.timeestimate ? (attributes.timeestimate / 3600 / 7) + 'd' : this.timeestimate;
+        this.effort = attributes[localStorage.getItem('storyPointsFieldId')] || this.effort;
+        if (attributes.issuelinks) {
+            attributes.issuelinks.forEach(this.registerLinksIssue.bind(this));
+        }
+    }
+
+    registerLinksIssue (issue) {
+        if (issue.outwardIssue) {
+            this.blocks.push(issue.outwardIssue.key);
+        }
+        if (issue.inwardIssue) {
+            this.isBlockedBy.push(issue.inwardIssue.key);
+        }
+    }
+
+    onSuccess (callback, response) {
+        this.parse(response.fields);
+        if (callback) {
+            callback(response);
+        }
+    }
+
+    fetch (callback) {
+        $.ajax({
+            url: `${localStorage.getItem('jiraRestApiBaseUrl')}/issue/${this.key}`,
+            dataType: 'json',
+            success: this.onSuccess.bind(this, callback),
+            error: function () {
+                alert('API error. Please verify the settings.');
+            }
+        });
     }
 }
 
@@ -61,52 +53,49 @@ class JiraView {
         this.model = model;
     }
 
-    decodeHTML (html) {
-        let domParser = new DOMParser();
-        let doc = domParser.parseFromString(html, "text/html");
-        return doc.documentElement.textContent;
+    bindElement () {
+        this.$element = document.querySelector(`#${this.model.key}`);
+        this.$element.querySelector('.remove').onclick = function () {
+            jiras.delete(this.model.key);
+        }.bind(this);
     }
 
     render () {
         this.element = `<article class="card" id="${this.model.key}">
-        		<h5 class="card-key">${this.model.key}</h5>
-                <h2 class="card-title">${this.decodeHTML(this.model.title)}</h2>
-                <div class="card-description"><h4>Description :</h4>${this.decodeHTML(this.model.description)}</div>
-                <div class="card-requirements">
-                <div class="requirement" data-requirement="token">Tokens</div>
-                <div class="requirement" data-requirement="UI">Validation UI</div>
-                <div class="requirement" data-requirement="TU">Tests unitaires</div>
-                <div class="requirement" data-requirement="TF">Tests fonctionnels</div>
-                <div class="requirement" data-requirement="PR">PR Green</div>
-                </div>
-            </article>`;
+        		<div class="card-key">${this.model.key}</div>
+                <h2 class="card-title">${this.model.summary}</h2>
+                <div class="card-description">${this.model.description}</div>
+                <div class="card-complexity">${this.model.effort || this.model.timeestimate || ''}</div>
+                <div class="card-links">`;
+        if (this.model.blocks.length) {
+            this.element += `<div class="card-blocks">Blocks: ${this.model.blocks.join(', ')}</div>`;
+        }
+        if (this.model.isBlockedBy.length) {
+            this.element += `<div class="card-isBlockedBy">Is blocked by: ${this.model.isBlockedBy.join(', ')}</div>`;
+        }
+        this.element += `</div><div class="remove">X</div></article>`;
         return this;
     }
 
     destroy () {
-
+        this.$element.remove();
     }
 }
 
 class JiraCollection {
     constructor () {
-        this.models = new Map();
-        this.views = new Map();
+        this.models  = new Map();
+        this.views   = new Map();
         this.element = document.getElementById('cards');
     }
 
-    _insertModel (attributes) {
-        if (!JiraModel.isValid(attributes)) {
-            return alert('XML invalide');
-        }
-        if (this.models.has(attributes.key)) {
+    _insertModel (model) {
+        if (this.models.has(model.key)) {
             return;
         }
-
-        let model = new JiraModel(attributes);
         let view = new JiraView(model);
-        this.models.set(attributes.key, model);
-        this.views.set(attributes.key, view);
+        this.models.set(model.key, model);
+        this.views.set(model.key, view);
         this.displayChildView(view);
     }
 
@@ -121,20 +110,90 @@ class JiraCollection {
     displayChildView (view) {
         view.render();
         this.element.innerHTML += view.element;
+        view.bindElement();
     }
 
     delete (key) {
         if (!this.models.has(key)) {
             return false;
         }
-        this.views.get(key).destoy();
+        this.views.get(key).destroy();
         this.views.delete(key);
         this.models.delete(key);
         return true;
     }
 }
 
-const $fileInput    = document.querySelector('#file')
-const jiras         = new JiraCollection();
+const jiras  = new JiraCollection();
+const $input = document.querySelector('#jiraKey');
+const $settingsContainer = document.querySelector('#settings-container');
+const $settingsURL = $settingsContainer.querySelector('#jiraBaseUrl');
+const $settingsStoryPoints = $settingsContainer.querySelector('#jiraStoryPoints');
 
-$fileInput.onchange = onUpload;
+$settingsURL.value = localStorage.getItem('jiraRestApiBaseUrl');
+$settingsStoryPoints.value = localStorage.getItem('storyPointsFieldName');
+
+var initModel = function (key) {
+    let jira = new JiraModel({
+        key: key
+    });
+    jira.fetch(function () {
+        jiras.add(jira);
+    });
+    $input.value = '';
+};
+
+var getStoryPointsFieldId = function (key) {
+    $.getJSON({
+        url: `${localStorage.getItem('jiraRestApiBaseUrl')}/field`,
+        dataType: 'json',
+        success: function (response) {
+            response.forEach(function (field) {
+                if (field.name === localStorage.getItem('storyPointsFieldName')) {
+                    localStorage.setItem('storyPointsFieldId', field.id);
+                    return false;
+                }
+                return true;
+            });
+            console.log(localStorage.getItem('storyPointsFieldId'));
+            initModel(key);
+        },
+        error: function () {
+            alert('API error. Please verify the settings.');
+        }
+    });
+};
+
+document.querySelector('#addJIRA').onsubmit = function () {
+    let key = $input.value;
+
+    if (!key) {
+        alert('Please enter a JIRA key.');
+    } else if (!localStorage.getItem('jiraRestApiBaseUrl')) {
+        alert('Please add a JIRA API BASE URL in the settings before continue.');
+    } else if (!localStorage.getItem('storyPointsFieldId')) {
+        getStoryPointsFieldId(key);
+    } else {
+        initModel(key);
+    }
+
+    return false;
+};
+
+var toggleSettings = function () {
+    $settingsContainer.classList.toggle('show');
+};
+
+document.querySelector('#settingsForm').onsubmit = function () {
+    localStorage.setItem('jiraRestApiBaseUrl', $settingsURL.value);
+    localStorage.setItem('storyPointsFieldName', $settingsStoryPoints.value);
+    localStorage.setItem('storyPointsFieldId', '');
+    toggleSettings();
+    return false;
+};
+
+document.querySelector('#print').onclick = function () {
+    window.print();
+};
+
+document.querySelector('#settings').onclick = toggleSettings;
